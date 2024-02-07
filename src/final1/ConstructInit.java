81ee71ad1b4cb6f645586caf8b3db5c64655aa88
dlehddud60 <dlehddud60@naver.com>
2024-02-07 09:04:44 +0900

    자바 final 예제

---
+++ src/final1/ConstructInit.java
@@ -0,0 +1,10 @@
+package final1;
+
+public class ConstructInit {
+    final int value;
+
+    public ConstructInit(int value) {
+        this.value = value;
+        //생성자를 통해 한번 초기화 하며 생성 이후에는 할당 불가
+    }
+}
